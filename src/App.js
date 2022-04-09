42d3c5fa1edc338122cd7cf53d341baf0327044a
Rejaur9131 <raju_seu@yahoo.com>
2022-04-09 06:17:14 +0600

    added on auth state change

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
+import Register from './components/Register/Register';
 import Home from './components/Home/Home';
 import Login from './components/Login/Login';
 
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
         <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="/register" element={<Register></Register>}></Route>
       </Routes>
     </div>
   );
